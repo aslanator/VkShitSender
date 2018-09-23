19f2d08ea2309646fe18011dd2a36edb526e26de
aslanator <aslanator@mail.ru>
2018-09-23 22:54:24 +0300

    work on front end

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,27 @@ function Shit() {
   this.head = document.querySelector('head');
 
   this.html = `
-  <div class="shit-search-button">
-    <div class="emoji_tt_wrap shit-tt-wrap"></div>
+  <div class="shit-search-container">
+    <div class="shit-search-button"></div>
+    <div class="emoji_tt_wrap shit-tt-wrap">
+      <div class="shit-result-container" id="shit-result-container">
+        <div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div>
+        <div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div>
+        <div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div><div class="shit-result-item"></div>
+      </div>
+      <div class="emoji_tabs shit-search-bottom clear_fix">
+        <div class="im-chat-input--txt-wrap _im_text_wrap">
+          <div class="im_editable im-chat-input--text _im_text" tabindex="0" contenteditable="true" id="shit-search-field" role="textbox" aria-multiline="false"></div>
+          <div class="placeholder"><div class="ph_input"><div class="ph_content">Поиск...</div></div></div>
+        </div>
+      </div>
+    </div>
   </div>
   `;
 
   this.addShitSearchButton = function () {
     this.buttonsContainer.insertAdjacentHTML('afterbegin', this.html);
+    new SimpleBar(document.getElementById('shit-result-container'));
 
     this.shitSearchButton = document.querySelector('.shit-search-button');
     this.shitTTwrap = document.querySelector('.shit-tt-wrap');
